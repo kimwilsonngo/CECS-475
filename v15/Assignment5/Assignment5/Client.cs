6ae9745a86f5042977880f90c43eb9d514c24e6d
Winn <winnmoo@gmail.com>
2018-11-07 15:34:58 -0800

    Recommit all files

---
+++ v15/Assignment5/Assignment5/Client.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace Client
+{
+    class Program
+    {
+        static BusinessLayer.BusinessLayer bl = new BusinessLayer.BusinessLayer();
+
+        public static void Main(string[] args)
+        {
+            string teacherName;
+            int teacherID;
+            int standardID;
+
+            string courseName;
+            int courseID;
+
+            bool doTeachers = false;
+            bool doCourses = false;
+
+            //Used to allow the menu to continue running
+            bool doGame = true;
+
+            int choice;
+            String num;
+
+
+            //While loop to continue displaying the main menu
+            while (doGame == true)
+            {
+                Console.WriteLine("Modify 1. Teachers or 2. Courses?");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    doTeachers = true;
+                    doCourses = false;
+                }
+                else if (choice == 2)
+                {
+                    doCourses = true;
+                    doTeachers = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+                while (doTeachers == true)
+                {
+                    menu1();
+                    Console.WriteLine("");
+                    //choice = Convert.ToInt32(Console.ReadLine());
+                    num = Console.ReadLine();
+                    choice = Int32.Parse(num);
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("- CREATE - ");
+                            Console.WriteLine("Enter Teacher Name: ");
+                            teacherName = Console.ReadLine();
+
+                            Console.WriteLine("Enter Standard ID: ");
+                            standardID = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            createTeacher(teacherName, standardID, teacherID);
+                            break;
+                        case 2:
+                            Console.WriteLine(" - UPDATE - ");
+                            Console.WriteLine("Enter Teacher Name: ");
+                            teacherName = Console.ReadLine();
+
+                            Console.WriteLine("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            updateTeacher(teacherName, teacherID);
+                            break;
+                        case 3:
+                            Console.WriteLine("- DELETE - ");
+                            Console.Write("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            deleteTeacher(teacherID);
+                            break;
+                        case 4:
+                            Console.WriteLine("- GET COURSES BY TEACHER ID");
+                            Console.Write("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            GetCourseByTeacherID(teacherID);
+                            break;
+                        case 5:
+                            Console.WriteLine("- GET ALL TEACHERS");
+                            GetAllTeachers();
+                            break;
+                        case 6:
+                            Console.WriteLine("- GET ALL STANDARDS -");
+                            //method call
+                            break;
+                        case 7:
+                            doTeachers = false;
+                            doCourses = false;
+                            break;
+                    }
+
+                }
+                //While loop to continue showing the menu for the courses
+                while (doCourses == true)
+                {
+                    menu2();
+                    Console.WriteLine("");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    //num = Console.ReadLine();
+                    //choice = Int32.Parse(num);
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("- CREATE COURSE -");
+                            Console.WriteLine("Enter Course Name: ");
+                            courseName = Console.ReadLine();
+
+                            Console.WriteLine("Enter Course ID: ");
+                            courseID = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            createCourse(courseName, courseID, teacherID);
+                            break;
+                        case 2:
+                            Console.WriteLine("- UPDATE COURSE -");
+                            Console.WriteLine("Enter Course Name: ");
+                            courseName = Console.ReadLine();
+
+                            Console.WriteLine("Enter Course ID: ");
+                            courseID = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Enter Teacher ID: ");
+                            teacherID = Convert.ToInt32(Console.ReadLine());
+                            updateCourse(courseName, courseID, teacherID);
+                            break;
+                        case 3:
+                            Console.WriteLine("- DELETE COURSE -");
+                            Console.WriteLine("Enter Course ID");
+                            courseID = Convert.ToInt32(Console.ReadLine());
+                            deleteCourse(courseID);
+                            break;
+                        case 4:
+                            Console.WriteLine("- DISPLAY ALL COURSES - ");
+                            getAllCourses();
+                            break;
+                        case 5:
+                            doTeachers = false;
+                            doCourses = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static void menu1()
+        {
+            Console.Write("1. CREATE " +
+                        "\n2. UPDATE " +
+                        "\n3. DELETE " +
+                        "\n4. LIST COURSES BY TEACHER ID " +
+                        "\n5. ALL TEACHERS " +
+                        "\n6. ALL STANDARDS " +
+                        "\n7. MAIN MENU");
+        }
+
+        /*
+         * Method to print the courses menu
+         *
+         */
+        private static void menu2()
+        {
+            Console.Write("1. CREATE " +
+                        "\n2. UPDATE " +
+                        "\n3. DELETE " +
+                        "\n4. ALL COURSES " +
+                        "\n5. MAIN MENU");
+        }
+
+        /*
+         * Method to create a teacher 
+         * @param teacher name, standard ID, teacher ID
+         */
+        private static void createTeacher(string tName, int sID, int tID)
+        {
+            Console.Write("Create \n");
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+
+            Teacher teacher = new Teacher();
+            teacher.TeacherName = tName;
+            teacher.StandardId = sID;
+            teacher.TeacherId = tID;
+            bl.AddTeacher(teacher); //this?
+
+        }
+        /*
+         * Method to update the teacher 
+         * @param teacher name, teacher ID
+         */
+        private static void updateTeacher(string tName, int cID)
+        {
+            Console.Write("Update \n");
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            Teacher teacher = bl.GetTeacherByID(cID);
+            teacher.TeacherName = tName;
+            //check function so it knows which one to update
+            bl.UpdateTeacher(teacher);
+
+        }
+
+        /*
+         * Method to delete the specified teacher
+         * @param teacherID
+         */
+        private static void deleteTeacher(int tID)
+        {
+            Console.Write("Delete \n");
+            Teacher teacher = bl.GetTeacherByID(tID);
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (teacher == null)
+            {
+                Console.WriteLine("Course does not exist!");
+                return;
+            }
+            bl.RemoveTeacher(teacher);
+        }
+
+        /*
+         * Method to return all courses taught by a teacher
+         * @param teacher ID
+         */
+        private static void GetCourseByTeacherID(int tID)
+        {
+            Console.WriteLine("Get Course By Teacher ID");
+            
+            foreach (Course i in bl.GetCourseByTeacherID(tID))
+            {
+     
+                Console.WriteLine(i.CourseName + " | cID: " + i.CourseId);
+            }
+        }
+        /*
+         * Method to return a list of all teachers
+         */
+        private static void GetAllTeachers()
+        {
+            Console.Write("All Teachers \n ");
+
+            IList<Teacher> list = bl.GetAllTeachers();
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (bl.GetAllTeachers() == null || bl.GetAllTeachers().Count == 0)
+            {
+                Console.WriteLine("List has no teachers!");
+                return;
+            }
+
+            Console.WriteLine("\nContaining Teachers: {0}", bl.GetAllTeachers().Count);
+            foreach (Teacher i in bl.GetAllTeachers())
+                Console.WriteLine("- " + i.TeacherName + " | tID: " + i.TeacherId + " | sID: " + i.StandardId);
+
+        }
+
+        /*
+         * Method to create a course
+         * @param Course name, course ID, teacher ID
+         * Teacher ID is used to specify what teacher is teaching the course
+         */
+        private static void createCourse(string cName, int cID, int tID)
+        {
+            Console.Write("Create Course \n");
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            Course course = new Course
+            {
+                CourseId = cID,
+                CourseName = cName,
+                TeacherId = tID
+            };
+            bl.AddCourse(course);
+            Console.WriteLine("Done");
+        }
+
+        /*
+         * Method to update a course
+         * @param Course name, course ID, teacher ID
+         */
+        private static void updateCourse(string cName, int cID, int tID)
+        {
+            Console.Write("Update Course \n");
+            Course course = bl.GetCourseByID(111);
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (course == null)
+            {
+                Console.WriteLine("Course does not exist!");
+                return;
+            }
+            course.CourseName = cName;
+            course.CourseId = cID;
+            course.TeacherId = tID;
+            bl.UpdateCourse(course);
+            Console.WriteLine("Course successfully updated");
+        }
+
+        /*
+         * Method to delete a course
+         * @param course ID
+         */
+        private static void deleteCourse(int cID)
+        {
+            Console.Write("Delete Course \n");
+            Course course = bl.GetCourseByID(cID);
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (course == null)
+            {
+                Console.WriteLine("Course does not exist!");
+                return;
+            }
+            bl.RemoveCourse(course);
+            Console.WriteLine("Course successfully removed");
+        }
+
+        /*
+         * Method to return all courses in the database
+         */
+        private static void getAllCourses()
+        {
+            Console.Write("Get all courses \n");
+
+            if (bl == null)
+            {
+                Console.WriteLine("Standard not found!");
+                return;
+            }
+            else if (bl.GetAllCourses() == null || bl.GetAllCourses().Count == 0)
+            {
+                Console.WriteLine("List has no Courses!");
+                return;
+            }
+
+            Console.WriteLine("\nContaining Courses: {0}", bl.GetAllCourses().Count);
+            foreach (Course i in bl.GetAllCourses())
+            {
+                Console.Write(i.CourseName + " | cID: " + i.CourseId + " | tID: " + i.TeacherId + "\n");
+            }
+        }
+    }
+}
---
+++ v15/Assignment5/BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,213 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BusinessLayer
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        private readonly IStandardRepository _standardRepository;
+        private readonly IStudentRepository _studentRepository;
+        private readonly ITeacherRepository _teacherRepository;
+        private readonly ICourseRepository _courseRepository;
+       
+
+        public BusinessLayer()
+        {
+            _standardRepository = new StandardRepository();
+            _studentRepository = new StudentRepository();
+            _teacherRepository = new TeacherRepository();
+            _courseRepository = new CourseRepository();
+            
+        }
+
+        #region Standard
+        public IEnumerable<Standard> GetAllStandards()
+        {
+            return _standardRepository.GetAll();
+        }
+
+        public Standard GetStandardByID(int id)
+        {
+            return _standardRepository.GetById(id);
+        }
+
+        public Standard GetStandardByName(string name)
+        {
+            return _standardRepository.GetSingle(
+                s => s.StandardName.Equals(name),
+                s => s.Students);
+        }
+
+        public void AddStandard(Standard standard)
+        {
+            _standardRepository.Insert(standard);
+        }
+
+        public void UpdateStandard(Standard standard)
+        {
+            _standardRepository.Update(standard);
+        }
+
+        public void RemoveStandard(Standard standard)
+        {
+            _standardRepository.Delete(standard);
+        }
+        //word doc
+        public Standard GetStandardByIDWithStudents(int id)
+        {
+            return _standardRepository.GetSingle(
+                s => s.StandardId == id,
+                s => s.Students);
+        }
+
+        //not implemented
+        IList<Standard> IBusinessLayer.GetAllStandards()
+        {
+            IList<Standard> getall = new List<Standard>();
+            foreach(Standard i in _standardRepository.GetAll())
+            {
+                getall.Add(i);
+            }
+
+            return getall; 
+        }
+
+
+        #endregion
+        public IList<Student> GetAllStudents()
+        {
+            IList<Student> getall = new List<Student>();
+            foreach (Student i in _studentRepository.GetAll())
+            {
+                getall.Add(i);
+            }
+
+            return getall;
+
+        }
+
+        public Student GetStudentByID(int id)
+        {
+            return _studentRepository.GetById(id);
+        }
+
+        public Student GetStudentByName(string name)
+        {
+            return _studentRepository.GetSingle(
+                s => s.StudentName.Equals(name),
+                s => s.Standard); //not sure if this line is right
+        }
+
+        public void AddStudent(Student student)
+        {
+            _studentRepository.Insert(student);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentRepository.Update(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            _studentRepository.Delete(student);
+        }
+
+
+
+
+        #region Student
+
+
+        #endregion
+
+        #region Teacher
+        public IList<Teacher> GetAllTeachers()
+        {
+            IList<Teacher> getall = new List<Teacher>();
+            foreach(Teacher i in _teacherRepository.GetAll())
+            {
+                getall.Add(i);
+            }
+
+            return getall;
+        }
+
+        public Teacher GetTeacherByID(int id)
+        {
+            return _teacherRepository.GetById(id);
+        }
+
+        public Teacher GetTeacherByName(string name)
+        {
+            return _teacherRepository.GetSingle(
+                s => s.TeacherName.Equals(name),
+                s => s.Standard);
+        }
+
+        public ICollection<Course> GetCourseByTeacherID(int id)
+        {
+            return _teacherRepository.GetById(id).Courses;
+        }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            _teacherRepository.Insert(teacher);
+        }
+
+        public void UpdateTeacher(Teacher teacher)
+        {
+            _teacherRepository.Update(teacher);
+        }
+
+        public void RemoveTeacher(Teacher teacher)
+        {
+            _teacherRepository.Delete(teacher);
+        }
+        #endregion
+
+        #region Course
+        public IList<Course> GetAllCourses()
+        {
+            IList<Course> getall = new List<Course>();
+            foreach(Course i in _courseRepository.GetAll())
+            {
+                getall.Add(i);
+            }
+            return getall;
+        }
+
+        public Course GetCourseByID(int id)
+        {
+            return _courseRepository.GetById(id);
+        }
+
+        public Course GetCourseByName(string name)
+        {
+            return _courseRepository.GetSingle(
+                s => s.CourseName.Equals(name),
+                s => s.CourseId);
+        }
+
+        public void AddCourse(Course course)
+        {
+            _courseRepository.Insert(course);
+            Console.WriteLine("added course");
+
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            _courseRepository.Update(course);
+
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            _courseRepository.Delete(course);
+        }
+        #endregion
+    }
+}
